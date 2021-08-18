3b29ec21163f3c12b692369fd7754fc3f9c5a966
kukolin <lucas@anezin.com>
2021-08-17 22:16:21 -0300

    Prueba Cantidad Turnos

--- Assets/Scripts/Tests/TestScript1.cs
+++ Assets/Scripts/Tests/TestScript1.cs
@@ -13,7 +13,7 @@ public class BowlingGameShould
     {
         bowlingGame = new BowlingGame();
         
-        int[] pineAmount = { 10, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        int[,] pineAmount = { { 3, 4 }, { 10, 0 }, { 5, 0 }, { 2, 0 }, { 3, 3 }, { 3, 4 }, { 10, 0 }, { 5, 5 }, { 2, 0 }, { 3, 3 } };
 
         bowlingGame.setTotalPineAmount(pineAmount);
     }
@@ -42,29 +42,42 @@ public class BowlingGameShould
         }
 
         //Then
-        Assert.AreEqual(false, exceedsMaxPineAmount);
+        Assert.IsFalse(exceedsMaxPineAmount);
     }
 
+    [Test]
+    public void ScoreOnTurnEqualsSumOfPines()
+    {
+
+        //When
+        int turnScore = bowlingGame.sumTurnPines(0);
+
+        //Then
+        Assert.AreEqual(7, turnScore);
+    }
 
 }
 
 public class BowlingGame 
 {
-    int[] totalPineAmount = new int[10];
+    int[,] totalPineAmount = new int[10,2];
     // int[] isStrike;
 
     public int getTurns() 
     {
-        return totalPineAmount.Length;
+        return totalPineAmount.GetLength(0);
     }
 
-    public int[] getTotalPineAmount() 
+    public int[,] getTotalPineAmount() 
     {
         return totalPineAmount;
     }
-    public void setTotalPineAmount(int[] pineAmount)
+    public void setTotalPineAmount(int[,] pineAmount)
     {
         this.totalPineAmount = pineAmount;
     }
-
+    public int sumTurnPines(int turn)
+    {
+        return totalPineAmount[turn, 0] + totalPineAmount[turn,1];
+    }
 }
